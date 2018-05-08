7dedbd317216e3b73933c24c7dd31054965c3105
Douglas Alves <douglas.expalves@gmail.com>
2018-05-08 02:20:31 -0300

    add frase-mock

---
+++ firstApp/src/app/painel/frase-mock.ts
@@ -0,0 +1,33 @@
+import { Frase } from '../utils/frase.model';
+
+export const frases: Array<Frase> = [
+  {
+    fraseEng: 'You are very pretty today',
+    fraseBr: 'Você está tão bonita hoje'
+  },
+  {
+    fraseEng: 'How old are you?',
+    fraseBr: 'Quantos anos você tem?'
+  },
+  {
+    fraseEng: 'You are so rude',
+    fraseBr: 'Você é tão rude'
+  },
+  {
+    fraseEng: `Mrs. Luna couldn't come to class today`,
+    fraseBr: 'Senhorita Luna não pode vir para a aula hoje'
+  },
+  {
+    fraseEng: `I'm not feeling well tonight`,
+    fraseBr: 'Eu não estou me sentido bem essa noite'
+  },
+  {
+    fraseEng: 'You are a very lucky person',
+    fraseBr: 'Você é uma pessoa de muita sorte'
+  },
+  {
+    fraseEng: 'I am a software developer',
+    fraseBr: 'Eu sou um desenvolvedor de software'
+  },
+];
+
