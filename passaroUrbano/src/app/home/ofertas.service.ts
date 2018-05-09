bd9911e84b4e187cc515b983941621b20fcea4f3
Douglas Alves <douglas.expalves@gmail.com>
2018-05-09 15:58:23 -0300

    update getOfertas to reach api

--- passaroUrbano/src/app/home/ofertas.service.ts
+++ passaroUrbano/src/app/home/ofertas.service.ts
@@ -1,55 +1,28 @@
+import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/toPromise';
+
 import { Oferta } from '../shared/oferta.model';
 
+@Injectable()
 export class OfertasService {
-  public getOfertas(): Array<Oferta> {
-    return [
-      {
-        id: 1,
-        categoria: 'restaurante',
-        titulo: 'Super Burger',
-        descricao_oferta: 'Rodízio de Mini-hambúrger com opção de entrada.',
-        anunciante: 'Original Burger',
-        valor: '29.90',
-        destaque: true,
-        imagens: [
-          {url: '/assets/ofertas/1/img1.jpg'},
-          {url: '/assets/ofertas/1/img2.jpg'},
-          {url: '/assets/ofertas/1/img3.jpg'},
-          {url: '/assets/ofertas/1/img4.jpg'}
-        ]
-      },
-      {
-        id: 2,
-        categoria: 'restaurante',
-        titulo: 'Cozinha Mexicana',
-        descricao_oferta: 'Almoço ou Jantar com Rodízio Mexicano delicioso.',
-        anunciante: 'Mexicana',
-        valor: '32.90',
-        destaque: true,
-        imagens: [
-          {url: '/assets/ofertas/2/img1.jpg'},
-          {url: '/assets/ofertas/2/img2.jpg'},
-          {url: '/assets/ofertas/2/img3.jpg'},
-          {url: '/assets/ofertas/2/img4.jpg'}
-        ]
-      },
-      {
-        id: 4,
-        categoria: 'diversao',
-        titulo: 'Estância das águas',
-        descricao_oferta: 'Diversão garantida com piscinas, trilhas e muito mais.',
-        anunciante: 'Estância das águas',
-        valor: '31.90',
-        destaque: true,
-        imagens: [
-          {url: '/assets/ofertas/3/img1.jpg'},
-          {url: '/assets/ofertas/3/img2.jpg'},
-          {url: '/assets/ofertas/3/img3.jpg'},
-          {url: '/assets/ofertas/3/img4.jpg'},
-          {url: '/assets/ofertas/3/img5.jpg'},
-          {url: '/assets/ofertas/3/img6.jpg'}
-        ]
-      }
-    ];
+  private ofertas: Array<Oferta> = [];
+
+  constructor(private http: Http) {}
+
+  public getOfertas(): Promise<Oferta[]> {
+    return new Promise((resolve, reject) => {
+      this.http.get('http://localhost:3000/' + 'ofertas' + '?destaque=true')
+        .toPromise()
+        .then((res: any) => res.json())
+        .then((ofertas: any) => {
+          this.ofertas = ofertas;
+          resolve(this.ofertas);
+        })
+        .catch((err: Error) => {
+          this.ofertas = [];
+          return reject(err);
+        });
+    });
   }
 }
