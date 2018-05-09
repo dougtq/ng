af1416bb53e6b18aa25edb5a6851fbf775cc3bc7
Douglas Alves <douglas.expalves@gmail.com>
2018-05-08 22:06:03 -0300

    make dynamic ofertas

--- passaroUrbano/src/app/home/home.component.ts
+++ passaroUrbano/src/app/home/home.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { OfertasService } from './ofertas.service';
+import { Oferta } from '../shared/oferta.model';
+
 @Component({
   selector: 'view-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [OfertasService]
 })
 export class HomeComponent implements OnInit {
+  public ofertas: Array<Oferta>;
 
-  constructor() { }
+  constructor(private ofertasService: OfertasService) {}
 
   ngOnInit() {
+    this.ofertas = this.ofertasService.getOfertas();
   }
 
 }
