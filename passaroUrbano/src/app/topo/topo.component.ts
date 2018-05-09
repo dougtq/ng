34ef94af38d1b36d30ffb8c0e272b9704d2e1cc7
Douglas Alves <douglas.expalves@gmail.com>
2018-05-09 04:27:45 -0300

    make topo nav dynamic

--- passaroUrbano/src/app/topo/topo.component.ts
+++ passaroUrbano/src/app/topo/topo.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { NavService } from './nav.service';
+import { Nav } from '../shared/nav.model';
 
 @Component({
   selector: 'view-topo',
   templateUrl: './topo.component.html',
-  styleUrls: ['./topo.component.css']
+  styleUrls: ['./topo.component.css'],
+  providers: [NavService]
 })
 export class TopoComponent implements OnInit {
+  navs: Array<Nav>;
 
-  constructor() { }
+  constructor(private navService: NavService) { }
 
   ngOnInit() {
+    this.navs = this.navService.getNavLinks();
   }
 
 }
