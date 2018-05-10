8d1956c431e297dc96a8fbf1685b34f1097a96e9
Douglas Alves <douglas.expalves@gmail.com>
2018-05-10 00:14:02 -0300

    create restaurantes component

--- passaroUrbano/src/app/restaurantes/restaurantes.component.ts
+++ passaroUrbano/src/app/restaurantes/restaurantes.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+
+import { OfertasService } from '../shared/ofertas.service';
+import { Oferta } from '../shared/oferta.model';
+
 @Component({
   selector: 'view-restaurantes',
   templateUrl: './restaurantes.component.html',
-  styleUrls: ['./restaurantes.component.css']
+  styleUrls: ['./restaurantes.component.css'],
+  providers: [OfertasService]
 })
 export class RestaurantesComponent implements OnInit {
+  public ofertas: Array<Oferta>;
+  public categoria = 'restaurante';
 
-  constructor() { }
+  constructor(private ofertaService: OfertasService) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    try {
+      this.ofertas = await this.ofertaService.getOfertasPorCategoria(this.categoria);
+    } catch (error) {
+      this.ofertas = [];
+    }
   }
 
 }
