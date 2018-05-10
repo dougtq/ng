2d933e17581713a3cec30b7fc9e5fb4aa2536133
Douglas Alves <douglas.expalves@gmail.com>
2018-05-10 00:14:20 -0300

    create diversao component

--- passaroUrbano/src/app/diversao/diversao.component.ts
+++ passaroUrbano/src/app/diversao/diversao.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { OfertasService } from '../shared/ofertas.service';
+import { Oferta } from '../shared/oferta.model';
+
 @Component({
   selector: 'view-diversao',
   templateUrl: './diversao.component.html',
-  styleUrls: ['./diversao.component.css']
+  styleUrls: ['./diversao.component.css'],
+  providers: [OfertasService]
 })
 export class DiversaoComponent implements OnInit {
+  public ofertas: Array<Oferta>;
+  public categoria = 'diversao';
+  constructor(private ofertaService: OfertasService) {}
 
-  constructor() { }
+  async ngOnInit() {
+    this.ofertas = await this.ofertaService.getOfertasPorCategoria(this.categoria);
 
-  ngOnInit() {
+    console.log(this.ofertas);
   }
-
 }
