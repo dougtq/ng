ddd0beb4bad5153331aecc8a36d464ba8c01e744
Douglas Alves <douglas.expalves@gmail.com>
2018-05-10 02:16:08 -0300

    create oferta route

--- passaroUrbano/src/app/app.routes.ts
+++ passaroUrbano/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RestaurantesComponent } from './restaurantes/restaurantes.component';
 import { DiversaoComponent } from './diversao/diversao.component';
+import { OfertaComponent } from './oferta/oferta.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'restaurantes', component: RestaurantesComponent },
-  { path: 'diversao', component: DiversaoComponent}
+  { path: 'diversao', component: DiversaoComponent},
+  { path: 'oferta/:id', component: OfertaComponent}
 ];
