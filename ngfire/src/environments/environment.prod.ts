1d6bfd44e8a55d7d76eb9342caf825c81ccbed29
Douglas Alves <douglas.expalves@gmail.com>
2018-05-11 16:38:31 -0300

    ngFire v1

--- ngfire/src/environments/environment.prod.ts
+++ ngfire/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAr_ELo6Gb-qq5nh2EBQzp7k22l7VP60c8',
+    authDomain: 'ngfire-tq.firebaseapp.com',
+    databaseURL: 'https://ngfire-tq.firebaseio.com',
+    projectId: 'ngfire-tq',
+    storageBucket: 'ngfire-tq.appspot.com',
+    messagingSenderId: '714083661452'
+  }
 };
--- ngfire/src/environments/environment.ts
+++ ngfire/src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAr_ELo6Gb-qq5nh2EBQzp7k22l7VP60c8',
+    authDomain: 'ngfire-tq.firebaseapp.com',
+    databaseURL: 'https://ngfire-tq.firebaseio.com',
+    projectId: 'ngfire-tq',
+    storageBucket: 'ngfire-tq.appspot.com',
+    messagingSenderId: '714083661452'
+  }
 };
