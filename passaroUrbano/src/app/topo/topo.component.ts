32dbdeeb4c58969a595c9b228128310f3c558fd2
Douglas Alves <douglas.expalves@gmail.com>
2018-05-12 06:11:01 -0300

    add rxjs operators switchMap and debounceTime

--- passaroUrbano/src/app/topo/topo.component.ts
+++ passaroUrbano/src/app/topo/topo.component.ts
@@ -1,10 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, ObservableInput } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
 
 import { NavService } from './nav.service';
 import { Nav } from '../shared/nav.model';
 import { OfertasService } from '../shared/ofertas.service';
 import { Oferta } from '../shared/oferta.model';
-import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/observable/of';
 
 @Component({
   selector: 'view-topo',
@@ -14,20 +19,31 @@ import { Observable } from 'rxjs/Observable';
 })
 export class TopoComponent implements OnInit {
   public navs: Array<Nav>;
-  public ofertas: Observable<Array<Oferta>>;
+  public ofertas: Array<Oferta>;
+  public subjectPesquisa: Subject<string> = new Subject<string>();
+  public observablePesquisa: Observable<Array<Oferta>>;
 
   constructor(private navService: NavService, private ofertaService: OfertasService) { }
 
   ngOnInit() {
     this.navs = this.navService.getNavLinks();
+    this.observablePesquisa = this.subjectPesquisa
+      .debounceTime(2000)
+      .switchMap((termo: string, i: number): ObservableInput<Oferta[]> => {
+        if (!termo.trim()) {
+          return Observable.of<Array<Oferta>>([]);
+        }
+        return this.ofertaService.pesquisaOferta(termo);
+      });
+
+    this.observablePesquisa.subscribe((ofertas: Array<Oferta>) => {
+      this.ofertas = ofertas;
+    });
   }
 
   public pesquisa(termo: string): void {
     // console.log((<HTMLInputElement>e.target).value);
-    this.ofertas = this.ofertaService.pesquisaOferta(termo);
-    this.ofertas.subscribe((ofertas: Array<Oferta>) => {
-      console.log(ofertas);
-    });
+    this.subjectPesquisa.next(termo);
   }
 
 }
