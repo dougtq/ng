1b377f0148e5bec030267a03e73bee8c167d9295
Douglas Alves <douglas.expalves@gmail.com>
2018-05-12 03:34:54 -0300

    update navbar component

--- ngfire/src/app/navbar/navbar.component.ts
+++ ngfire/src/app/navbar/navbar.component.ts
@@ -6,7 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  public title = 'NgFire';
+  public routes = [
+    {
+      link: 'angular',
+      name: 'Angular'
+    },
+    {
+      link: 'vue',
+      name: 'Vue'
+    },
+    {
+      link: 'react',
+      name: 'React'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
