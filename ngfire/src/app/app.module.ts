daab7522b2b9cad9fbb7af183e72786aabe509fd
Douglas Alves <douglas.expalves@gmail.com>
2018-05-12 03:35:56 -0300

    update app module with new modules and components

--- ngfire/src/app/app.module.ts
+++ ngfire/src/app/app.module.ts
@@ -3,21 +3,37 @@ import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CoursesListComponent } from './courses-list/courses-list.component';
+import { RoutingModule } from './routing/routing.module';
+import { AngularComponent } from './angular/angular.component';
+import { VueComponent } from './vue/vue.component';
+import { ReactComponent } from './react/react.component';
+import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CoursesListComponent,
+    AngularComponent,
+    VueComponent,
+    ReactComponent,
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    AngularFireAuthModule
+    AngularFireAuthModule,
+    NgbModule.forRoot(),
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
