f37db43c7afe7972e0dd68d6184bb78fcf072b66
Douglas E. Alves <douglas.expalves@gmail.com>
2018-06-17 18:25:18 -0300

    simple form example with reactive forms

--- form-fire/src/app/conteudo/cadastro/cadastro.component.ts
+++ form-fire/src/app/conteudo/cadastro/cadastro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cadastro.component.css']
 })
 export class CadastroComponent implements OnInit {
-
+  public formulario: FormGroup = new FormGroup({
+    'email': new FormControl(null, [Validators.required, Validators.email]),
+    'password': new FormControl(null, [Validators.required, Validators.minLength(6), Validators.maxLength(20)]),
+    'check': new FormControl(null)
+  });
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public enviaCadastro(): void {
+    console.log(this.formulario);
   }
 
 }
